810e52266a3aab166a72e87c8948f4d0ff4efc05
esther <esthernnyamburaa@gmail.com>
2025-03-27 11:00:24 +0300

    unit testing

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,10 +1,4 @@
-// const fizzBuzz =require('./fizzbuzz.js')
 
-// test('if 15%3===0 && 15%5===0 equal to fuzzbuzz',() => {
-//     expect(fizzBuzz (15)).toBe("Fizz") ;
-//     expect(fizzBuzz (15)).toBe("Buzz");
-//     expect (fizzBuzz(45)).toBe("Fizz");
-// })
 const fizzBuzz = require("./fizzbuzz")
 test ('bring Fizz when its divided by 3', ()=>{
     expect(fizzBuzz(12)).toBe("Fizz");
